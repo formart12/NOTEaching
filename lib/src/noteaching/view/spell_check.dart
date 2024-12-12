7c277a8f712b673ae4b7ae10342ea08646dc3c4a
formart12 <formart12@naver.com>
2024-12-12 21:25:31 +0900

    first commit

---
+++ lib/src/noteaching/view/spell_check.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SpellCheck extends StatelessWidget {
+  const SpellCheck({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
